02639600300717d9fc62422f203185b6823c099b
Tunc Celik <celiktunc44@gmail.com>
2020-10-06 20:59:49 +0100

    Added newwwwwwwwwwwwwwwwwwww file for awesome feature

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,13 @@
+public class AwesomeTest {
+
+    //red means it is untracked
+    //green means it is under staging area
+    //grey is .idea folder means it is ignored
+    //when the color is normal. it means it is committed no changes
+
+    //i just pulled the new change from github that I did not have locally
+    //and now my project is in synic with remote repository
+    
+    // Now I made more awesome changes and Iam about to push this back to github
+}
+
